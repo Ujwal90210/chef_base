4a931f4a2c4fb580212e3c7cd3581c11d7bfef9c
ujwaly <ujwal.yelmareddy@fox.com>
2018-01-30 00:15:25 -0800

    updated Jenkins file updates

--- cookbooks/chef_base/metadata.rb
+++ cookbooks/chef_base/metadata.rb
@@ -6,5 +6,8 @@ maintainer_email 'ujwal90210@gmail.com'
 license 'Apache-2.0'
 version '1.0.0'
 
-depends 'docker'
-depends 'chef-client'
+depends 'docker' #FC064
+depends 'chef-client' #FC066
+
+#FC065
+#FC067
